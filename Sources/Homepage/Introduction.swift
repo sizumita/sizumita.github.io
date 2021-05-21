eb9d10f34e6ffbda1f0e9b06d8abb65096243f0d
sizumita <sumito@izumita.com>
2021-05-21 21:18:23 +0530

    Change to auto generate age

--- Sources/Homepage/Introduction.swift
+++ Sources/Homepage/Introduction.swift
@@ -1,12 +1,17 @@
 import TokamakDOM
+import Foundation
 
 struct IntroductionView: View {
     var body: some View {
+        let calendar = Calendar(identifier: .gregorian)
 
-        VStack {
+        let birthday = calendar.date(from: DateComponents(year: 2003, month: 5, day: 26, hour: 0, minute: 0, second: 0))
+        let elapsedDays = Calendar.current.dateComponents([.year], from: birthday!, to: Date()).year
+
+        return VStack {
             Text("長野県安曇野市在住")
                 .padding()
-            Text("2003年5月26日生まれの17歳です。")
+            Text("2003年5月26日生まれの" + String(elapsedDays!) + "歳です。")
                 .padding()
             Text("関数型言語が好きですが、Pythonが一番書けます。最近はElixirを書いています。")
                 .padding()
